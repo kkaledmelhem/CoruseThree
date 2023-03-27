b127a86365abeb84a090fc8bec482f648f68cd77
Abdelrahman Ibrahim <ajibrahim19@cit.just.edu.jo>
2023-03-28 01:40:56 +0300

    yousef dam3a

---
+++ CoruseThree/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoruseThree.Models
+{
+    public class Car
+    {
+        public int MyProperty { get; set; }
+    }
+}
